409b82c138953d940e9e1c508b8d95a008f8e7ac
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-10-27 18:14:44 +0530

    contactlist props

---
+++ src/components/Contactcard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cardcontact = () => {
+    return(
+        <div className="item">
+       <div className="contact">
+         <div className="header"> {contact.name}</div>
+         <div>{contact.email}</div>
+         </div>
+         <i className="trash alternate outline icon"></i>
+       </div>
+    )
+}
--- src/components/Contactlist.js
+++ src/components/Contactlist.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Contactlist = () => {
+const Contactlist = (props) => {
+  console.log(props);
+
+  const renderContactlist= props.Contacts.map(contact => {
+    return(
+      <div className="item">
+       <div className="contact">
+         <div className="header"> {contact.name}</div>
+         <div>{contact.email}</div>
+         </div>
+         <i className="trash alternate outline icon"></i>
+       </div>
+    );
+  });
+  
+
   return (
-<div className="ui celled list">contact list</div>  )
-}
+<div className="ui celled list">{renderContactlist}</div>)
+};
 
 export default Contactlist;

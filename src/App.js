871c0df63f69ceea73a9ddfd5be5e678c903af98
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-10-28 15:41:11 +0530

    Another attempt to fix deployment failure.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { userstat } from "react";
+import React, { userstat, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Addcontact from  "./components/Addcontact";
 import Contactlist from "./components/Contactlist";
 
 function App() {
-  const[contacts, setcontact]= userstat([])
+  const[contacts, setcontact]= useState([])
   
   return( 
   <div className="ui container">

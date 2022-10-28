00c8f4c7c05eaee8eb32018c026248b550dba978
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-10-28 15:47:16 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { userstat, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Addcontact from  "./components/Addcontact";
 import Contactlist from "./components/Contactlist";
 
 function App() {
-  const[contacts, setContact]= useState([])
-  
+  const [contacts, setContacts]= useState([])
   return( 
   <div className="ui container">
     <Header />

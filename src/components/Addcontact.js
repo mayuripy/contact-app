07937a2a67445e7f86bdef785992e90c09bafafa
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-10-29 16:10:30 +0530

    abcd

--- src/components/Addcontact.js
+++ src/components/Addcontact.js
@@ -4,12 +4,18 @@ class Addcontact extends Component{
   state ={
     name:"",
     email:"",
+  };
+
+  add = (e) => {
+    e.preventdefault();
+    if(this.state.name)
+
   }
  render(){
     return(
         <div className="ui main">
           <h2>Add contact</h2>
-          <form className="ui form">
+          <form className="ui form"onSubmit={this.add}>
             <div className="field">
               <label>Name</label>
                <input type="text"
@@ -23,8 +29,8 @@ class Addcontact extends Component{
                <input type="text"
                 name="email"
                  placeholder="email"
-                 value={this.state.name} 
-                 onChange={ (e) => this.setState({name: e.target.v})}
+                 value={this.state.email} 
+                 onChange={ (e) => this.setState({email: e.target.v})}
                  />
             </div>
             <button className="ui button blue">Add</button>

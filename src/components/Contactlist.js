85ca63148321616eb86387dc6af3d2e3077e7dae
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-10-31 23:04:50 +0530

    Some Changes

--- src/components/Contactlist.js
+++ src/components/Contactlist.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Contactcard from "./Contactcard.js";
 
 const Contactlist = (props) => {
@@ -6,17 +7,28 @@ const Contactlist = (props) => {
 
   const deleteContactHandler = (id) =>{
     props.getcontactId(id);
-  }
+  };
+  
 
   const renderContactlist= props.contacts.map((contact) => {
     return(
-      <Contactcard contact={contact} clickHander ={deleteContactHandler}/>
+      <Contactcard contact={contact} clickHander ={deleteContactHandler} key={contact.id}/>
     );
   });
   
 
   return (
+    <div class="main">
+      <h2>
+        Contact list
+        <link to="/add">
+        <button className="ui button blue right"> Add contact</button>
+        </link>
+      </h2>
 <div className="ui celled list">{renderContactlist}</div>)
+</div>
+  );
 };
+
 
 export default Contactlist;
